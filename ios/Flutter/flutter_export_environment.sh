b78a23e22fcae8c982361fcdfa136c53cde67535
Kevin Yobeth <leokeviny@gmail.com>
2019-11-21 23:26:49 +0700

    First Commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Kevin Claudine\Documents\Library\Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Project\Flutter\flutterLogin"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Kevin Claudine\Documents\Library\Flutter\bin\cache\artifacts\engine\ios"
